eacaf074201e93b9eaec6c881c68707afdf326e5
Edwin Bensoussan <edwin.bensoussan@student.ecp.fr>
2016-04-02 13:04:10 +0200

    init commit

---
+++ MyCVWebApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using MyCVWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCVWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return View("Error");
+            else
+                ViewData["Nom"] = id;
+                return View("index");
+        }
+
+
+        // GET: Home
+        public string Index2(int id)
+        {
+            return "Hello world " + id;
+        }
+
+
+        public ActionResult ListeClients()
+        {
+            Clients clients = new Clients();
+            ViewData["Clients"] = clients.ObtenirListeClients();
+            return View();
+        }
+
+        public ActionResult ChercheClient(string id)
+        {
+            ViewData["Nom"] = id;
+            Clients clients = new Clients();
+            Client client = clients.ObtenirListeClients().FirstOrDefault(c => c.Nom == id);
+            if (client != null)
+            {
+                ViewData["Age"] = client.Age;
+                return View("Trouve");
+            }
+            return View("NonTrouve");
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# MyCVWebApp
+=======
+# MyCVWebApp
+>>>>>>> d8c62c9f439a64048f2325468879d0ee75d166ef
+# MyCVWebApp
